4eec2825d1928087bf29122664c564557f100c13
owen-d <ow.diehl@gmail.com>
2015-02-22 19:09:08 -0500

    almost there

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var Twit = require('twit');
-var heap = require('heap');
+var transform = require('../transform');
 
 var screenName = 'arhythmetric';
+var SIXTEEN_MINUTES_IN_MS = 1000 * 60 * 16;
 var app = express();
 
 //Routes n whatnot
@@ -20,22 +21,35 @@ var twit = new Twit({
   access_token_secret: process.env.TWIT_TOKEN_SECRET
 });
 
+var lastCursor;
 var counted = 0;
 var users = [];
 var recursiveSearch = function(err, data, response){
   if (err) {
     console.log('search broken, count @ time=' + counted, '\nerror:\n', err);
-    console.log('users array:', users);
-    // setTimeout(recursiveSearch, 1000*60*16)
+    // console.log('users array:', users);
+
+    setTimeout(function(){
+      console.log('attempting again, hopefully not still blocked');
+      twit.get('followers/list', {screen_name: screenName, next_cursor: lastCursor}, recursiveSearch);
+    }, SIXTEEN_MINUTES_IN_MS);
+
+    console.log('next round in 16 minutes from now,', new Date());
+
   } else {
     counted+= data.users.length;
     console.log('counted so far:', counted);
+    lastCursor = data['next_cursor_str'];
     //add tuple with user name & follower count to our users array
-    users.push(data.users.map(function(item, index, array){
-      return [item['screen_name'], item['followers_count']];
+    users = users.concat(data.users.map(function(item, index, array){
+      return {handle: item['screen_name'], followers: item['followers_count']};
     }));
-    console.log('users array:', users);
-    // twit.get('followers/list', {screen_name: screenName, next_cursor: data['next_cursor_str']}, recursiveSearch);
+
+    if (lastCursor === 0) {
+      transform(users);
+    } else {
+      twit.get('followers/list', {screen_name: screenName, next_cursor: data['next_cursor_str']}, recursiveSearch);
+    }
   }
 };
 
---
+++ transform.js
@@ -0,0 +1,49 @@
+var babyparse = require('babyparse');
+var Heap = require('heap');
+var Stream = require('stream');
+var fs = require('fs');
+var jsoncsv = require('json-csv');
+
+module.exports = function(users){
+  var hp = new Heap(function(a,b){
+    return b.followers - a.followers;
+  });
+
+  while(users.length) {
+    hp.push(users.pop());
+  }
+
+  var readStream = new Stream.Readable({objectMode: true});
+  readStream._read = function(){
+    var temp = hp.pop();
+    if (temp === undefined) {
+      readStream.push(null);
+    } else {
+      readStream.push(temp);
+      // console.log(temp.toString());
+    }
+  };
+
+
+  var csvOptions = {
+    fields: [{
+      name: 'handle',
+      label: 'Handle',
+      quoted: true
+    }, {
+      name: 'followers',
+      label: 'Number of Followers'
+    }]
+  };
+
+  var output = fs.createWriteStream('output.csv', {encoding: 'utf8'});
+  readStream.pipe(jsoncsv.csv(csvOptions)).pipe(output);
+  output.on('finish', function(){
+    console.log('Finished writing to csv.');
+  });
+};
+
+
+
+
+
