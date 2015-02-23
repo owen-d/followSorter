912a17165c3b026c3713b788b43320044fa943dd
owen-d <ow.diehl@gmail.com>
2015-02-23 10:04:19 -0500

    refactoring to OAuth & idSearch

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var Twit = require('twit');
 var transform = require('../transform');
+var OAuth = require('oauth');
 
-var screenName = 'arhythmetric';
-var SIXTEEN_MINUTES_IN_MS = 1000 * 60 * 16;
+var screenName = process.env.SCREEN_NAME;
+var THREE_MINUTES_IN_MS = 1000 * 60 * 3;
 var app = express();
 
 //Routes n whatnot
@@ -13,68 +13,61 @@ var app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-//twit stuff
-var twit = new Twit({
-  consumer_key: process.env.TWIT_CONSUMER_KEY,
-  consumer_secret: process.env.TWIT_CONSUMER_SECRET,
-  access_token: process.env.TWIT_ACCESS_TOKEN,
-  access_token_secret: process.env.TWIT_TOKEN_SECRET
-});
+var oa = new OAuth.OAuth(
+  'https://api.twitter.com/oauth/request_token',
+  'https://api.twitter.com/oauth/access_token',
+  process.env.TWIT_CONSUMER_KEY,
+  process.env.TWIT_CONSUMER_SECRET,
+  '1.0A',
+  null,
+  'HMAC-SHA1'
 
-var lastCursor;
+);
+
+var lastCursor = -1;
 var counted = 0;
 var users = [];
 var recursiveSearch = function(err, data, response){
   if (err) {
-    console.log('search broken, count @ time=' + counted, '\nerror:\n', err);
-    // console.log('users array:', users);
+    console.log('search interrupted, count @ time=' + counted, '\nerror:\n', err);
 
     setTimeout(function(){
       console.log('attempting again, hopefully not still blocked');
-      twit.get('followers/list', {screen_name: screenName, next_cursor: lastCursor}, recursiveSearch);
-    }, SIXTEEN_MINUTES_IN_MS);
+      sendRequest();
+    }, THREE_MINUTES_IN_MS);
 
-    console.log('next round in 16 minutes from now,', new Date());
+    console.log('next round in 3 minutes from now,', new Date());
 
   } else {
-    counted+= data.users.length;
+    data = JSON.parse(data);
+    counted+= data.ids.length;
     console.log('counted so far:', counted);
     lastCursor = data['next_cursor_str'];
+    console.log('lastCursor:', lastCursor);
     //add tuple with user name & follower count to our users array
-    users = users.concat(data.users.map(function(item, index, array){
-      return {handle: item['screen_name'], followers: item['followers_count']};
-    }));
+    users = users.concat(data.ids);
 
-    if (lastCursor === 0) {
-      transform(users);
+    if (lastCursor === '0') {
+      // transform(users);
+      console.log('end of users!!!');
     } else {
-      twit.get('followers/list', {screen_name: screenName, next_cursor: data['next_cursor_str']}, recursiveSearch);
+      sendRequest();
     }
   }
 };
 
 
 
+function sendRequest (){
+  oa.get(
+    'https://api.twitter.com/1.1/followers/ids.json?screen_name='+screenName+'&cursor='+lastCursor,
+    process.env.TWIT_ACCESS_TOKEN,
+    process.env.TWIT_TOKEN_SECRET,
+    recursiveSearch
+  );
+};
 
-twit.get('followers/list', { screen_name: screenName },  recursiveSearch);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+sendRequest();
 
 
 
